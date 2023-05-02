6b1dc9905a990c04963872677cee640daf6b0cfd
theo0x1337 <theo.bernardin@gmail.com>
2023-05-02 23:16:27 +0200

    resolving conflicts

--- src/app/vouch/vouch.service.ts
+++ src/app/vouch/vouch.service.ts
@@ -3,11 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vouch } from './vouch';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class VouchService {
-  private apiServerUrl = "http://localhost:8080";
+  private apiServerUrl = "http://3.70.232.79:5000";
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) { }
 
   public getVouch(): Observable<Vouch[]> {
     return this.http.get<Vouch[]>(`${this.apiServerUrl}/vouch/all`);
